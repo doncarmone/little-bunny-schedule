a9676207bbd49466b34319f16c8960962deb4905
Javier Carmona <javier.carmona@wizeline.com>
2025-09-17 13:16:24 -0600

    [ADD] removed gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/little-bunny-schedule',
-  assetPrefix: '/little-bunny-schedule/'
 };
 
 export default nextConfig;
